85336d7d811427c9f2e2a7cdc8d2bb99db97ae55
Josh0wah <134548428+Josh0wah@users.noreply.github.com>
2025-04-18 21:05:44 -0500

    Add files via upload
    Implemented SRA amd SLA instructions

--- gbe/src/main.rs
+++ gbe/src/main.rs
@@ -143,6 +143,8 @@ enum Instruction {
     RL(ArithmeticTarget),
     RRC(ArithmeticTarget),
     RLC(ArithmeticTarget),
+    SRA(ArithmeticTarget),
+    SLA(ArithmeticTarget),
 }
 
 enum ArithmeticTarget {
@@ -843,6 +845,73 @@ impl CPU {
                 }
             }
 
+            Instruction::SRA(target) => {
+                match target {
+                    ArithmeticTarget::A => {
+                        let new_value = self.sra('a');
+                        self.registers.a = new_value;
+                    }
+                    ArithmeticTarget::B => {
+                        let new_value = self.sra('b');
+                        self.registers.b = new_value;
+                    }
+                    ArithmeticTarget::C => {
+                        let new_value = self.sra('c');
+                        self.registers.c = new_value;
+                    }
+                    ArithmeticTarget::D => {
+                        let new_value = self.sra('d');
+                        self.registers.d = new_value;
+                    }
+                    ArithmeticTarget::E => {
+                        let new_value = self.sra('e');
+                        self.registers.e = new_value;
+                    }
+                    ArithmeticTarget::H => {
+                        let new_value = self.sra('h');
+
+                        self.registers.h = new_value;
+                    }
+                    ArithmeticTarget::L => {
+                        let new_value = self.sra('l');
+                        self.registers.l = new_value;
+                    }
+                }
+            }
+
+            Instruction::SLA(target) => {
+                match target {
+                    ArithmeticTarget::A => {
+                        let new_value = self.sla('a');
+                        self.registers.a = new_value;
+                    }
+                    ArithmeticTarget::B => {
+                        let new_value = self.sla('b');
+                        self.registers.b = new_value;
+                    }
+                    ArithmeticTarget::C => {
+                        let new_value = self.sla('c');
+                        self.registers.c = new_value;
+                    }
+                    ArithmeticTarget::D => {
+                        let new_value = self.sla('d');
+                        self.registers.d = new_value;
+                    }
+                    ArithmeticTarget::E => {
+                        let new_value = self.sla('e');
+                        self.registers.e = new_value;
+                    }
+                    ArithmeticTarget::H => {
+                        let new_value = self.sla('h');
+                        self.registers.h = new_value;
+                    }
+                    ArithmeticTarget::L => {
+                        let new_value = self.sla('l');
+                        self.registers.l = new_value;
+                    }
+                }
+            }
+
         }
     }
 
@@ -1336,6 +1405,58 @@ impl CPU {
             false =>
                 new_value,
         }
+    }
+
+    //sra function
+    //Arithmetic shift right
+    fn sra(&mut self, target_register: char) -> u8 {
+        let target = match target_register{
+            'a' => self.registers.a,
+            'b' => self.registers.b,
+            'c' => self.registers.c,
+            'd' => self.registers.d,
+            'e' => self.registers.e,
+            'h' => self.registers.h,
+            'l' => self.registers.l,
+            _ => panic!("unknown register value")
+        };
+
+        let mask = target & 0x80;
+
+        let mut new_value = (target >> 1) | mask;
+
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = false;
+        self.registers.f.half_carry = (target <= 0xF) & (new_value > 0xF);
+
+        new_value
+    }
+
+    //sla function
+    //Arithmetic shift left
+    fn sla(&mut self, target_register: char) -> u8 {
+        let target = match target_register{
+            'a' => self.registers.a,
+            'b' => self.registers.b,
+            'c' => self.registers.c,
+            'd' => self.registers.d,
+            'e' => self.registers.e,
+            'h' => self.registers.h,
+            'l' => self.registers.l,
+            _ => panic!("unknown register value")
+        };
+
+        let mask = target & 0x80;
+
+        let mut new_value = (target << 1) & mask;
+
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = false;
+        self.registers.f.half_carry = (target <= 0xF) & (new_value > 0xF);
+
+        new_value
     }
 
 }
